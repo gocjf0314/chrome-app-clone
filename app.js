12c503da6f4fa673f4af2d0cb5209443e4e3518b
Hae Cheol Jeong <40590800+gocjf0314@users.noreply.github.com>
2023-07-04 01:36:56 +0900

    Save basic level source code

--- app.js
+++
@@ -1,7 +0,0 @@
-const h1 = document.querySelector(".hello h1");
-const clickedClass = "clicked";
-function handleTitleClick() {
-  h1.classList.toggle(clickedClass);
-}
-
-h1.addEventListener("click", handleTitleClick);
